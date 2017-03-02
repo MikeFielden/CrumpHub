ec19d4a3f8861a96abb332aea938567ac74caa34
Mike Fielden <fielden.mike@gmail.com>
2017-03-02 08:13:51 -0600

    Removing unused/commented out code

--- js/displayCurrentDateAndTime.js
+++ js/displayCurrentDateAndTime.js
@@ -1,39 +1,11 @@
-
-function displayCurrentDate ()  {
-    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    , currentDate = new Date()
-    ,  dayofWeek = currentDate.getDay()    
-    ,  printDay = weekdays[dayofWeek]
-    ,  day = currentDate.getDate()
-    ,  month = currentDate.getMonth() + 1
-    ;
-$('.currentDate').html(moment().format('dddd, MMMM D, YYYY'))
-//  $('.currentDate').html('<p> It is ' + printDay + ', ' + month + '/' + day + ' </p>');
+function displayCurrentDate() {
+  $('.currentDate').html(moment().format('dddd, MMMM D, YYYY'))
 }
 
-function displayCurrentTime () {
-var currentTime = new Date()
-  , hours = currentTime.getHours()
-  , minutes = currentTime.getMinutes()
-  , suffix = "AM"
-  ;
-
-    if (minutes < 10) {
-	   minutes = "0" + minutes;
-    }
-
-	if (hours >= 12) {
-        suffix = "PM";
-        hours = hours - 12;
-	}
-	if (hours == 0) {
-	   hours = 12;
-	}
-
-   /* $('.currentTime').html('<p> ' + hours + ':' + minutes + ' ' + suffix + ' </p>');   */
-    $('.currentTime').html(moment().format('LT'))
+function displayCurrentTime() {
+  $('.currentTime').html(moment().format('LT'))
 }
 
 displayCurrentDate();
 displayCurrentTime();
-setInterval(displayCurrentTime, 1000*60);
+setInterval(displayCurrentTime, 1000 * 60);

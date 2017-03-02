c55ec72cc776f55f695e33cf1fa2e8a99e0367e7
Mike Fielden <fielden.mike@gmail.com>
2017-03-02 08:24:37 -0600

    Made some code review changes

--- js/ameliaChecklist.js
+++ js/ameliaChecklist.js
@@ -1,55 +1,61 @@
 var weekdayChecklistForAmelia = [
-    'Eat Breakfast'
-    , 'Put Dishes in the Sink'
-    , 'Brush Teeth'
-    , 'Get Dressed'
-    , 'Clean up Toys'
-    , 'Help Clean the Table After Dinner'
-    , 'Clean Room'
-    , 'Take a Bath'
-    , 'Brush Teeth'
-    , 'Put on Pajamas'
-    , 'Get Ready for Bed'
+  'Eat Breakfast'
+  , 'Put Dishes in the Sink'
+  , 'Brush Teeth'
+  , 'Get Dressed'
+  , 'Clean up Toys'
+  , 'Help Clean the Table After Dinner'
+  , 'Clean Room'
+  , 'Take a Bath'
+  , 'Brush Teeth'
+  , 'Put on Pajamas'
+  , 'Get Ready for Bed'
 ];
 
 var weekendChecklistForAmelia = [
-    'Help Put Away Breakfast Dishes'
-    , 'Brush Teeth'
-    , 'Get Dressed'
-    , 'Help Clean Bathroom'
-    , 'Help Clean Living Room'
-    , 'Put Away Any Toys That Are Out'
-    , 'Help Clear the Table'
-    , 'Take a Bath'
-    , 'Brush Teeth'
-    , 'Put on Pajamas'
-    , 'Get Ready for Bed'
+  'Help Put Away Breakfast Dishes'
+  , 'Brush Teeth'
+  , 'Get Dressed'
+  , 'Help Clean Bathroom'
+  , 'Help Clean Living Room'
+  , 'Put Away Any Toys That Are Out'
+  , 'Help Clear the Table'
+  , 'Take a Bath'
+  , 'Brush Teeth'
+  , 'Put on Pajamas'
+  , 'Get Ready for Bed'
 ];
 
-function printChecklist( ) {
+/**
+ *
+ * @param listOfThings
+ */
+function printChecklist($spotToPutIt, listOfThings) {
   var listHTML = '<ol>';
-  for ( var i = 0; i <= weekdayChecklistForAmelia.length -1; i ++) {
-    listHTML += '<li>' + weekdayChecklistForAmelia[i] + '</li>';
+  for (var i = 0; i <= listOfThings.length - 1; i++) {
+    listHTML += '<li>' + listOfThings[i] + '</li>';
   }
   listHTML += '</ol>';
-  $('.ameliaChecklist').html(listHTML);
-} 
-
-function printWeekendChecklist( ) {
-  var listHTML = '<ol>';
-  for ( var i = 0; i <= weekendChecklistForAmelia.length -1; i ++) {
-    listHTML += '<li>' + weekendChecklistForAmelia[i] + '</li>';
-  }
-  listHTML += '</ol>';
-  $('.ameliaChecklist').html(listHTML);
-} 
-
-currentDate = new Date()
-,  dayofWeek = currentDate.getDay();
-if (dayofWeek > 0 && dayofWeek < 6 ) {
-  printChecklist(); 
-}  else {
-   printWeekendChecklist(); 
-    
+  
+  // You might have done this on accident but this is the exact right way
+  $spotToPutIt.html(listHTML);
 }
 
+/**
+ * Gets what items to display based on whether the day of the week is a
+ *   weekday or a weekend
+ *
+ * @type {Number} dayOfWeek
+ */
+function getItemsToDisplayBasedOffTypeOfDay(dayOfWeek) {
+  return (dayOfWeek > 0 && dayOfWeek < 6
+    ? weekdayChecklistForAmelia
+    : weekendChecklistForAmelia);
+}
+
+var currentDate             = new Date()
+  , checklistItemsToDisplay = getItemsToDisplayBasedOffTypeOfDay(currentDate.getDay())
+  , $ameliaChecklist        = $('.ameliaChecklist')
+  ;
+
+printChecklist($ameliaChecklist, checklistItemsToDisplay);
